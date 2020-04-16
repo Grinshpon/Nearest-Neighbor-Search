0d71fdb412430171e92a09130ed438856aa592ae
grinshpon <grinshpondaniel@protonmail.com>
2020-04-16 18:14:42 -0400

    remove debug stuff

--- src/project.nim
+++ src/project.nim
@@ -17,32 +17,5 @@ when isMainModule:
     qPoint: Vec[DIM] = [4.0,5.0]
     nPoint = tree.nearestSearch(qPoint, euclidean_squared[DIM])
 
-  echo $tree
+  #echo $tree
   tree.plotTree2(qPoint, nPoint, -1000.0,1000.0)
-
-
-when false:
-  #echo $data
-  # -------------------------------------------------
-  let v: Vec[DIM] = [1.0,2.0]
-  let t: Tree[DIM] = kdt.new_tree[DIM](v)
-  echo $t
-
-  let
-    p1: Vec[DIM] = [0.0,-1.0]
-    p2: Vec[DIM] = [1.0,2.0]
-    p3: Vec[DIM] = [0.5,0.0]
-    p4: Vec[DIM] = [0.0,1.0]
-    p5: Vec[DIM] = [0.0,0.0]
-
-  var
-    pts1 = @[p1,p2,p3]
-    pts2 = @[p4,p1,p2,p3]
-    pts3 = @[p1,p2,p3,p4]
-
-  #echo $kdt.treeFromPoints(pts2)
-  let tree = kdt.treeFromPoints(pts3)
-  echo $tree
-  echo $tree.nearestSearch(p5,euclidean_squared[DIM])
-  # --------------------------------------------------
-
